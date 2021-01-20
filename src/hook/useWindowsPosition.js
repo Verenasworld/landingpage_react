d3d6972d66a0ee83e3daca6215dc57225cef9767
Verenasworld <verena.felizidas@gmail.com>
2021-01-20 15:53:22 +0100

    fix bug dynamisches laden content und animation

--- src/hook/useWindowsPosition.js
+++ src/hook/useWindowsPosition.js
@@ -1,19 +1,18 @@
-import {useLayoutEffect, useState} from "react";
+import { useLayoutEffect, useState } from 'react';
 
-export default function useWindowsPosition(id) {
+export default function useWindowPosition(id) {
     const [animation, setAnimation] = useState(false);
 
     useLayoutEffect(() => {
         function updatePosition() {
-            const offSetHeight = window.document.getElementById(id).offsetHeight;
-            if (window.pageXOffset > offSetHeight * 0.7) {
+            const offetSetHeight = window.document.getElementById(id).offsetHeight;
+            if (window.pageYOffset > offetSetHeight * 0.7) {
                 setAnimation(true);
             }
         }
-
-        window.addEventListener("scroll", updatePosition);
+        window.addEventListener('scroll', updatePosition);
         updatePosition();
-        return () => window.removeEventListener("scroll", updatePosition);
+        return () => window.removeEventListener('scroll', updatePosition);
     }, [id]);
     return animation;
 }
